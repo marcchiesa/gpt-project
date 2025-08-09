95439de1b49f237ba0397bea9886de5f33f2d0c4
marcchiesa <marcchiesa@protonmail.com>
2025-08-09 23:19:32 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="robots" content="noindex, nofollow" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
