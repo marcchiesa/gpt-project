b34d40377430bc9661848f44e70bd60628ef50ea
marcchiesa <marcchiesa@protonmail.com>
2025-08-09 23:41:43 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+```tsx
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import '../styles/globals.css'
@@ -8,11 +9,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="robots" content="noindex, nofollow" />
       </Head>
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 space-y-8">
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 space-y-8 prose prose-lg">
         <Component {...pageProps} />
       </main>
     </div>
   )
 }
-
-// Added "space-y-8" to main container to create vertical spacing between elements and remove visible \n characters.
+```
